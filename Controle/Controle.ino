0af4a961bb679f01c1f0ab0989412b82a7229177
JairoBraga <neto.braga08@gmail.com>
2019-10-19 18:52:40 -0300

    Adicionado a função de calibração

--- Controle/Controle.ino
+++ Controle/Controle.ino
@@ -5,7 +5,8 @@
 #endif
 
 MPU6050 accelgyro;
-
+const int g_rate = 131;
+const int a_rate = 16384;
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
 void setup() {
@@ -24,17 +25,26 @@ void setup() {
     accelgyro.initialize();
     Serial.println("Testing device connections...");
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+    calibrateMPU();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
   accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
   Serial.print("a/g:\t");
   Serial.print(ax); Serial.print("\t");
   Serial.print(ay); Serial.print("\t");
   Serial.print(az); Serial.print("\t");
-  Serial.print(gx/131); Serial.print("\t");
-  Serial.print(gy/131); Serial.print("\t");
-  Serial.println(gz/131);
+  Serial.print(gx/g_rate); Serial.print("\t");
+  Serial.print(gy/g_rate); Serial.print("\t");
+  Serial.println(gz/g_rate);
   delay(100);
+}
+
+void calibrateMPU(){
+  accelgyro.CalibrateGyro(6);
+  accelgyro.CalibrateAccel(6);
+  Serial.println("Inciando calibração");
+  accelgyro.PrintActiveOffsets();
 }
