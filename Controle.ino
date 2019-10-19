5becdc43f12053b46734a16e80a9e73270e5c6ad
JairoBraga <neto.braga08@gmail.com>
2019-10-19 18:31:16 -0300

    Adicionado as bibliotecas do sensor e comunicação e primeiros comandos de leitura

---
+++ Controle.ino
@@ -0,0 +1,40 @@
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+MPU6050 accelgyro;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+void setup() {
+  // put your setup code here, to run once:
+
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+     Serial.begin(38400);
+
+    // initialize device
+    Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+  Serial.print("a/g:\t");
+  Serial.print(ax); Serial.print("\t");
+  Serial.print(ay); Serial.print("\t");
+  Serial.print(az); Serial.print("\t");
+  Serial.print(gx/131); Serial.print("\t");
+  Serial.print(gy/131); Serial.print("\t");
+  Serial.println(gz/131);
+  delay(100);
+}
